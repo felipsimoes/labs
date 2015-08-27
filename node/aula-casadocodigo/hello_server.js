26d15f592b57f90824b17b5d23bfd7d4ad2a1457
Felipe Simões <felipe_lima_simoes@hotmail.com>
2015-08-27 11:10:33 -0300

    servidor com variaveis e funcoes bem definidas

--- node/aula-casadocodigo/hello_server.js
+++ node/aula-casadocodigo/hello_server.js
@@ -1,10 +1,15 @@
 var http = require('http');
 
-var server = http.createServer(function(request, response){
+var atendeRequisicao = function(request, response){
 	response.writeHead(200, {"Content-Type": "text/html"});
 	response.write("<h1>Hello World!</h1>");
 	response.end();
-});
-server.listen(3000, function(){
+}
+
+var server =  http.createServer(atendeRequisicao);
+
+var servidorLigou = function(){
 	console.log('Servidor Hello World rodando!');
-});
+}
+
+server.listen(3000, servidorLigou);
