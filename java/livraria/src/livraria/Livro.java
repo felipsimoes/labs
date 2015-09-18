396ee2318d882126c25d06f97b81f355d4725e4e
Felipe Lima Simões <felipe_lima_simoes@hotmail.com>
2015-09-17 23:29:58 -0300

    subindo projeto de classe Livro

---
+++ java/livraria/src/livraria/Livro.java
@@ -0,0 +1,19 @@
+package livraria;
+
+public class Livro {
+	String nome;
+	String descricao;
+	double valor;
+	String isbn;
+	
+	void mostrarDetalhes(){
+		System.out.println("Mostrando detalhes do livro");
+		System.out.println("Nome "+nome);
+		System.out.println("Descrição "+descricao);
+		System.out.println("Valor" +valor);
+		System.out.println("ISBN" + isbn);
+		System.out.println("--");
+	}
+}	
+
+
