646d94c1159a9f35a5d2634b436f9b7829e93b51
Felipe Lima Simões <felipe_lima_simoes@hotmail.com>
2015-09-18 15:34:04 -0300

    encapsulando atributos e métodos

--- java/livraria/src/livraria/Livro.java
+++ java/livraria/src/livraria/Livro.java
@@ -1,11 +1,11 @@
 package livraria;
 
 public class Livro {
-	String nome;
-	String descricao;
-	double valor;
-	String isbn;
-	Autor autor;
+	private String nome;
+	private String descricao;
+	private double valor;
+	private String isbn;
+	private Autor autor;
 	
 	
 	public void mostrarDetalhes(){
@@ -22,13 +22,60 @@ public class Livro {
 		System.out.println("--");
 	}
 	
-	public void aplicaDesconto(double porcentagem){
+	public boolean aplicaDesconto(double porcentagem){
+		if (porcentagem > 0.3) {
+			System.out.println("Desconto não pode ser maior do que 30%");
+			return false;
+		}
 		this.valor -= this.valor * porcentagem;
+		return true;
 	}
 	
 	public boolean temAutor(){
 		return this.autor != null;
 	}
+		
+	public double getValor(){
+		return this.valor;
+	}
+	
+	public void setValor(double valor){
+		this.valor = valor;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public String getIsbn() {
+		return isbn;
+	}
+
+	public void setIsbn(String isbn) {
+		this.isbn = isbn;
+	}
+
+	public Autor getAutor() {
+		return autor;
+	}
+
+	public void setAutor(Autor autor) {
+		this.autor = autor;
+	}
+	
+	
 }	
 
 
